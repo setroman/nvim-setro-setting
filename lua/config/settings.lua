0b1b6a450b8b2d7b4b9c410e2a2d4dcf34e280bc
Jorge <setroman78@gmail.com>
2024-04-17 20:40:55 -0300

    Agrego gitsigns y seteo la tecla leader.

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,5 +2,6 @@
 --
 vim.o.number = true 		-- Número de línea visible
 vim.o.relativenumber = true	-- Numeración relativa activada
+vim.g.mapleader = " "		-- cambio el la tecla <leader> por defecto a un espacio 
 
 -- vim.cmd.colorscheme("slate") -- Esquema de colores (tema)
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,13 @@
+-- Extension signs para colorear cambios en los archivos
+--
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		numhl = true,	-- colorea el número de la linea.
+		max_file_length = 10000, -- limita el tamaño mazimo de un atchivo para habrirlo.
+	}
+	-- config = true,
+	-- congig = funcion()
+	-- 	require("gitsings").setup()
+	-- end
+}
