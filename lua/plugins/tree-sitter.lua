3cede542f91d342432075bf8e248ed1bebf150c4
Jorge <setroman78@gmail.com>
2024-04-20 21:46:31 -0300

    Corrijo el archivo y aguegue vimdoc

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,33 +1,37 @@
 -- Extención generadora de parcer para el manejo del arbol de sintaxis.
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	enent = "VeryLazy",
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = {
-			"lua",
-			"luadoc",
-		},
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				loogahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["af"] = "@conditional.outer",
-					["if"] = "@conditional.inner",
-					["af"] = "@loop.outer",
-					["if"] = "@loop.inner",
-				},
-			},
-		},
-	},
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  build = ":TSUpdate",
+  enent = "VeryLazy",
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "lua",
+            "luadoc",
+            "vimdoc", -- Lo agregue porque parece arregle los errores de treesitter en el help
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@conditional.outer",
+          ["ic"] = "@conditional.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+        },
+      },
+    },
+  },
 }
