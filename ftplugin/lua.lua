39f227e566d068a8c3d6fc181163192192c6582d
Jorge <setroman78@gmail.com>
2024-04-20 21:47:50 -0300

    Agrego la configuracion de tabs a dos espacios

---
+++ ftplugin/lua.lua
@@ -0,0 +1,7 @@
+-- Seteo de tipografia
+
+-- Set tab
+vim.bo.tabstop = 2 -- Número de espacio por tab
+vim.bo.softtabstop = 2 -- Número de espacios por tab insertados
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
