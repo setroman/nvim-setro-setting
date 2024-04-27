cc9931ce7f3aba80dfb9ef240b8de5fc11e52d27
Jorge <setroman78@gmail.com>
2024-04-27 15:17:52 -0300

    + typscritp-language-server y parametrice el archivo un poco

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,53 +1,68 @@
 -- Extencion de configuracion de LSP
---
+
+local config = function ()
+  -- Redefino la funcion keymap.set, y las tablas lsp y api para que quede más límpio la lectura
+  local kms, lsp = vim.keymap.set, vim.lsp
+  local lspconfig = require("lspconfig")
+
+  -- Global mappings.
+  -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+  kms('n', '<space>e', vim.diagnostic.open_float)
+  kms('n', '[d', vim.diagnostic.goto_prev)
+  kms('n', ']d', vim.diagnostic.goto_next)
+  kms('n', '<space>q', vim.diagnostic.setloclist)
+
+  local on_attach = function(_, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = bufnr }
+    kms('n', 'gD', lsp.buf.declaration, opts)
+    kms('n', 'gd', lsp.buf.definition, opts)
+    kms('n', 'K', lsp.buf.hover, opts)
+    kms('n', 'gi', lsp.buf.implementation, opts)
+    kms('n', '<C-k>', lsp.buf.signature_help, opts)
+    kms('n', '<space>wa', lsp.buf.add_workspace_folder, opts)
+    kms('n', '<space>wr', lsp.buf.remove_workspace_folder, opts)
+    kms('n', '<space>wl', function()
+      print(vim.inspect(lsp.buf.list_workspace_folders()))
+    end, opts)
+    kms('n', '<space>D', lsp.buf.type_definition, opts)
+    kms('n', '<space>rn', lsp.buf.rename, opts)
+    kms({ 'n', 'v' }, '<space>ca', lsp.buf.code_action, opts)
+    kms('n', 'gr', lsp.buf.references, opts)
+    kms('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end
+
+  require("neodev").setup()
+
+  -- LSP config -----------------------------------------------
+  --
+  --typescript and javascript congig
+  lspconfig.tsserver.setup({
+    on_attach = on_attach
+  })
+
+  -- lua config
+  lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    settings = {
+      lua = {
+        telemetry = { enable = false },
+        workspace = { checkThirParty = false },
+      }
+    }
+  })
+end
+
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "williamboman/mason.nvim", -- Debe correr primero mason
     "folke/neodev.nvim",       -- Configura automaticamente Lua lsp
   },
-  config = function()
-    -- Global mappings.
-    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-    local on_attach = function(_, bufnr)
-      -- Enable completion triggered by <c-x><c-o>
-      vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-      -- Buffer local mappings.
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      local opts = { buffer = bufnr }
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-      vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-      vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, opts)
-      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-      vim.keymap.set('n', '<space>f', function()
-        vim.lsp.buf.format { async = true }
-      end, opts)
-    end
-
-    require("neodev").setup()
-    require("lspconfig").lua_ls.setup({
-      on_attach = on_attach,
-      settings = {
-        lua = {
-          telemetry = { enable = false },
-          workspace = { checkThirParty = false },
-        }
-      }
-    })
-  end
+  config = config
 }
