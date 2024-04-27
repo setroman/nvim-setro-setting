af820afbb782bee6dafba2b7606ba6d1354e6b58
Jorge <setroman78@gmail.com>
2024-04-27 18:39:31 -0300

    + markdown-preview.nvim

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,8 @@
+-- markdown-preview.nvim para previsualizar marckdown
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
+
